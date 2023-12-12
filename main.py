230bfe4213f01ddd1c6061df5fec5b4ca6c473c1
Ryan Gordon <ryangordon@duck.com>
2023-12-12 16:28:40 +1300

    Remove really annoying delay and refactor a couple of things

--- main.py
+++ main.py
@@ -5,26 +5,23 @@
 # Prints out dad jokes at random until the user terminates the program
 
 import random
-from time import sleep
 import json
+from time import sleep
 
 if __name__ == "__main__":
-    # Load the jokes.
     jokes = json.load(open("jokes.json"))
     random.shuffle(jokes)
 
     print("Welcome to Dad Jokes :) ")
-    input("Press ctrl+c to exit at any time (but pls don't)")
+    print("Press ctrl+c to exit at any time (but pls don't)")
 
     try:
-        # Loop until all jokes in the jokes() function have been told
         for joke in jokes:
             print(f"\n{joke[0]}")
             input("Press enter to find out the answer... ")
             print(joke[1])
-            sleep(2)
 
         print("\nWe're out of jokes sorry, but we'll have more soon :D\n")
-    # There are two ways to end the program, print an appropriate message
+        print("(Pssst, you could create a pull request with more jokes...)")
     except KeyboardInterrupt:
         print("\nSorry to lose you before the end, come again soon!\n")
